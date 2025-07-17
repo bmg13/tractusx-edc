22b0e019678f88bf0bee5b9533ee0a32e5de28b3
bmg13 <gabcmonteiro@gmail.com>
2025-07-17 11:15:38 +0100

    Fix several headers.

--- edc-extensions/dataplane/dataplane-http/src/test/java/org/eclipse/edc/connector/dataplane/http/pipeline/HttpDataSourceFactoryTest.java
+++ edc-extensions/dataplane/dataplane-http/src/test/java/org/eclipse/edc/connector/dataplane/http/pipeline/HttpDataSourceFactoryTest.java
@@ -1,20 +1,21 @@
-/*
- *  Copyright (c) 2021 Microsoft Corporation
+/**
+ * Copyright (c) 2025 Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
  *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
  *
- *  SPDX-License-Identifier: Apache-2.0
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
  *
- *  Contributors:
- *       Microsoft Corporation - initial API and implementation
- *       Amadeus - test retrieval of auth code from vault
- *       Amadeus - add test for mapping of path segments
- *       Mercedes Benz Tech Innovation - add toggles for proxy behavior
- *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - add functionalities - improvements
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
- */
+ * SPDX-License-Identifier: Apache-2.0
+ **/
 
 package org.eclipse.edc.connector.dataplane.http.pipeline;
 
